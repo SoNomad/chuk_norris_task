a1c2f73c081627867cc17e16d7112bd2fefe79ee
SoNomad <nurmuhammad16203@gmail.com>
2022-09-29 14:28:46 +0300

    Release version

---
+++ norris.js
@@ -0,0 +1,15 @@
+const fn = async () => {
+  const response = await fetch("https://api.chucknorris.io/jokes/random");
+  const jokeObject = await response.json();
+  jokeText.textContent = jokeObject.value;
+};
+
+const btn = document.createElement("button");
+btn.textContent = "say";
+const jokeContainer = document.querySelector(".joke_div");
+const jokeText = document.createElement("div");
+jokeContainer.append(jokeText, btn);
+
+btn.addEventListener("click", () => {
+  fn();
+});
